79eed25c157b63139f4cf83e05a99abe0d2dc636
Developer Nahida <ummenahida20@gmail.com>
2025-05-25 22:08:52 +0600

    solve the all problems

--- index.sql
+++ index.sql
@@ -10,7 +10,7 @@ CREATE Table species(
     common_name VARCHAR(100) NOT NULL,
     scientific_name VARCHAR(150) NOT NULL,
     discovery_date DATE NOT NULL,
-    conservation_status VARCHAR(50) CHECK (conservation_status IN ('Endangered', 'Vulnerable', 'Near Threatened', 'Least Concern'))
+    conservation_status VARCHAR(50) CHECK (conservation_status IN ('Endangered', 'Vulnerable', 'Near Threatened', 'Least Concern','Historic'))
 )
 
 
@@ -26,7 +26,7 @@ CREATE Table sightings(
 
 );
 
-DROP Table rangers;
+DROP Table species;
 
 INSERT INTO rangers (name, region) VALUES
 ('Alice Green', 'Northern Hills'),
@@ -54,7 +54,7 @@ SELECT * FROM species;
 SELECT * FROM sightings; 
 
 --------------------Problem-1 Register a new ranger ------------
-INSERT INTO rangers(ranger_id,name,region) VALUES('Derek Fox','Coastal Plains');
+INSERT INTO rangers(name,region) VALUES('Derek Fox','Coastal Plains');
 
 
 ---------------------Problem-2 Count unique species-----------
@@ -66,7 +66,38 @@ SELECT * FROM sightings WHERE location LIKE('%Pass%');
 
 
 ------------------Problem 4--------------
+SELECT r.name,count(s.ranger_id) FROM rangers r JOIN sightings s on r.ranger_id = s.ranger_id GROUP BY r.name;
 
 
+----------------Problem 5--------
+SELECT * FROM rangers WHERE ranger_id NOT IN (SELECT species_id FROM sightings); 
+
+----------------Problem 6----------
+-- filtering by time 
+SELECT spc.common_name,s.sighting_time, name
+FROM sightings s JOIN species spc ON s.sighting_id = spc.species_id
+JOIN rangers r ON r.ranger_id = s.species_id
+ORDER BY sighting_time DESC LIMIT 2;
+
+-----------------Problem 7.
+UPDATE species
+ set conservation_status='Historic'
+ WHERE discovery_date < '1800-01-01';
+
+------------------Problem 8 ----------
+SELECT 
+  sighting_id,
+  sighting_time,
+  sighting_time::time AS only_time,
+  CASE 
+    WHEN sighting_time::time < TIME '12:00' THEN 'Morning'
+    WHEN sighting_time::time >= TIME '12:00' AND sighting_time::time <= TIME '17:00' THEN 'Afternoon'
+    ELSE 'Evening'
+  END AS time_of_day
+FROM sightings;
+
+
+-------------------problem 9
+DELETE FROM rangers WHERE ranger_id NOT IN (SELECT species_id FROM sightings)
 
 
